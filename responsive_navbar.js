69d91e3f8fbaf640538a5ce320af603515ec784c
Miguel Silva <miguelklogsilva@gmail.com>
2025-11-21 10:36:10 +0000

    todo: correct some html tags and put  css all in one file

--- responsive_navbar.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const links = document.querySelector('.nav-links');
-
-toggle.addEventListener('click', () => {
-    links.classList.toggle('show');
-});
